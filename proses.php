824ef34e45ab9e3dc0e4dc51aca91331dbc6c0ad
AnindiaAgusta <126650401+AnindiaAgusta@users.noreply.github.com>
2023-03-28 12:46:34 +0700

    v1.2.1

--- proses.php
+++ proses.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hasil Perhitungan</title>
+</head>
+<body>
+	<?php
+		//mengambil nilai dari form
+		$angka1 = $_POST['angka1'];
+		$angka2 = $_POST['angka2'];
+		$operasi = $_POST['operasi'];
 
+		//melakukan operasi aritmatika sesuai dengan nilai pada dropdown
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $angka1 + $angka2;
+				break;
+			case 'kurang':
+				$hasil = $angka1 - $angka2;
+				break;
+			case 'kali':
+				$hasil = $angka1 * $angka2;
+				break;
+			case 'bagi':
+				$hasil = $angka1 / $angka2;
+				break;
+			default:
+				echo "Pilih operasi terlebih dahulu";
+				return;
+		}
+
+		//menampilkan hasil perhitungan
+		echo "Hasil perhitungan dari $angka1 $operasi $angka2 adalah $hasil";
+	?>
+</body>
+</html>
